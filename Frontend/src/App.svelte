0d66e718a641fd21ba82f78f77d8ebead3c90ef2
Macky151203 <91668861+Macky151203@users.noreply.github.com>
2022-03-26 14:26:05 +0530

    Update App.svelte

--- Frontend/src/App.svelte
+++ Frontend/src/App.svelte
@@ -1,18 +1,42 @@
-<script>
-	export let name;
-</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	
+	<div class="center">
+        <h1 class="heading">login</h1>
+        <form method="post">
+            <div class="text_field">
+                <input type="text" required placeholder="Enter your username*">
+                <label for=""></label>
+            </div>
+            <div class="text_field">
+                <input type="password" required placeholder="Enter youe password*">
+                <label for=""></label>
+            </div>
+            <div class="pass">forgot password?</div>
+            <input type="submit" value="login">
+            <div class="sign_uplink">
+                not a member? <a href="#">sign up</a>
+            </div>
+        </form>
+    </div>
+	
 </main>
 
 <style>
+	
 	main {
 		text-align: center;
-		padding: 1em;
+		/* padding: 1em; */
 		max-width: 240px;
-		margin: 0 auto;
+		margin: 0 ;
+        padding: 0 ;
+        background: black;
+        height: 100vh;
+        overflow: hidden;
 	}
 
 	h1 {
@@ -27,4 +51,57 @@
 			max-width: none;
 		}
 	}
+    .heading{
+        color: wheat;
+    }
+    .form{
+        padding: 0 40px;
+    }
+    .text_field{
+        position: relative;
+        
+        margin: 30px 0;
+    }
+    .text_field input{
+        width: 25%;
+        padding: 0 5px;
+        height: 30px;
+        border-radius: 10px;
+    }
+    .text_field label{
+        position: absolute;
+        top: 50%;
+        /* left: 5px; */
+        color: #413d3d;
+    }
+    input[type="submit"]{
+        width: 7%;
+        /* height: 30px; */
+        border: 1px solid #6d777e;
+        background: #2691d9;
+        border-radius: 10px;
+        cursor: pointer;
+
+    }
+    .signup_link{
+        margin: 30px 0;
+        text-align: center;
+        font-size: 16px;
+        color: #a6a6a6;
+}
+.signup_link a{
+  color: #2691d9;
+  text-decoration: none;
+}
+.signup_link a:hover{
+  text-decoration: underline;
+}
+.pass{
+  margin: -5px 0 20px 5px;
+  color: wheat;
+  cursor: pointer;
+}
+.pass:hover{
+  text-decoration: underline;
+}
 </style>
